1f5773ef8252a1ed763298f0fb52b80031ffb9ea
ZKA <zaki_le_kids@hotmail.fr>
2023-08-28 13:26:10 +0200

    suppression fichier inutile

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Logements from './pages/Logements';
-import About from './pages/About';
 import Error from './pages/Error';
 import './sass/main.scss';
 
@@ -14,7 +13,6 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Logements/:id" element={<Logements />} />
-        <Route path='/About' element={<About />} />
         <Route path='/*' element={<Error />} />
       </Routes>
     </Router>
